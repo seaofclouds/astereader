63f3bfab975d908f80e79c9ad1357967f6f8c38e
seaofclouds <seaofclouds@users.noreply.github.com>
2023-07-02 21:58:25 -0700

    move spaceship controls to usespaceship.js

--- src/components/GameSpace.js
+++ src/components/GameSpace.js
@@ -2,42 +2,61 @@ import React, { useState, useEffect, useRef } from "react";
 import ScrollingText from "./ScrollingText";
 import Bullet from "./Bullet";
 import {useBullet} from "./useBullet"; 
+import useSpaceship from './useSpaceship';
 
+// Main Functional Component
 const GameSpace = () => {
+
+  // State Variables
   const [gameState, setGameState] = useState("running");
   const [text, setText] = useState("");
-  const [rotation, setRotation] = useState(0);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const spaceship = useRef();
   const [bullets, setBullets] = useBullet([], 5); // bulletSpeed is 5
   const [score, setScore] = useState(0);
-  const velocity = useRef({x: 0, y: 0});
-  const acceleration = useRef({x: 0, y: 0});
-  const speed = 0.05; // control this to change the spaceship's speed
-  const [thrust, setThrust] = useState(0);
-  const [lives, setLives] = useState(3);
-const [gameId, setGameId] = useState(0);
+  const [gameId, setGameId] = useState(0);
+  const {
+	  rotation,
+	  acceleration,
+	  setRotation,
+	  velocity,
+	  speed,
+	  thrust,
+	  setThrust,
+	  x,
+	  setX,
+	  y,
+	  setY,
+	  lives,
+	  setLives,
+	  spaceship,
+	  handleKeyDown,
+	  handleKeyUp
+} = useSpaceship(setBullets);
+	  
+  useEffect(() => {
+	  window.addEventListener('keydown', handleKeyDown);
+	  window.addEventListener('keyup', handleKeyUp);
+  
+	  return () => {
+		window.removeEventListener('keydown', handleKeyDown);
+		window.removeEventListener('keyup', handleKeyUp);
+	  }
+	}, [rotation, x, y, thrust]);
 
-  // Add a function to reset the game state and variables
-  const restartGame = () => {
-	setGameState("running");
-	setLives(3);
-	setScore(0);
-	setBullets([]);
-	setText("");
-	setX(window.innerWidth / 2 - spaceship.current.offsetWidth / 2);
-	setY(window.innerHeight / 2 - spaceship.current.offsetHeight / 2);
-	setRotation(0);
-	velocity.current = {x: 0, y: 0};
-	acceleration.current = {x: 0, y: 0};
-	setThrust(0);
-	document.body.classList.remove('game-over');
-	setGameId(gameId => gameId + 1); 
-	
-  };
 
-const highlightRandomWords = (text) => {
+  // Function to reset the game state and variables
+  // Reset game state and use the useSpaceship hook to reset spaceship variables
+	const restartGame = () => {
+	  setGameState("running");
+	  setScore(0);
+	  setBullets([]);
+	  setText("");
+	  spaceship.reset(); // Resets spaceship state via useSpaceship hook
+	  document.body.classList.remove('game-over');
+	  setGameId(gameId => gameId + 1);
+	};
+
+  // Function to Highlight Random Words 
+  const highlightRandomWords = (text) => {
 	const words = text.split(' ');
 	for (let i = 0; i < words.length; i++) {
 	  // Randomly decide whether to highlight this word
@@ -47,247 +66,148 @@ const highlightRandomWords = (text) => {
 	  }
 	}
 	return words.join(' ');
- };
+  };
 
+  // UseEffect Hooks
+  // Fetch text data on game start
   useEffect(() => {
-	  if (gameState === "running") {
-		fetch("/content.txt")
-		  .then((response) => response.text())
-		  .then((data) => setText(highlightRandomWords(data)));
-	  }
-	}, [gameState, gameId]);
+	if (gameState === "running") {
+	  fetch("/content.txt")
+		.then((response) => response.text())
+		.then((data) => setText(highlightRandomWords(data)));
+	}
+  }, [gameState, gameId]);
 
-
+  // Reset spaceship position on game start
   useEffect(() => {
-		if (gameState === "running") {
-		  setX(window.innerWidth / 2 - spaceship.current.offsetWidth / 2);
-		  setY(window.innerHeight / 2 - spaceship.current.offsetHeight / 2);
-		}
-	  }, [gameState]);
+	  
+	if (gameState === "running") {
 
+	  setX(window.innerWidth / 2 - spaceship.current.offsetWidth / 2);
+	  setY(window.innerHeight / 2 - spaceship.current.offsetHeight / 2);
+	}
+  }, [gameState]);
+
+
+  // Check collision with words every 50ms
   useEffect(() => {
-	  if (gameState === "running") {
-
-	const handleKeyDown = (e) => {
-	  e.preventDefault();
-	  switch(e.key) {
-		case 'ArrowLeft':
-		  setRotation(rotation => rotation - 10);
-		  break;
-		case 'ArrowRight':
-		  setRotation(rotation => rotation + 10);
-		  break;
-		case 'ArrowUp':
-			let radian = rotation * Math.PI / 180;
-			setThrust(0.1); // Begin applying thrust when the up arrow is pressed
-
-			acceleration.current = {
-				x: -speed * Math.sin(-radian),
-				y: -speed * Math.cos(-radian)
-			};
-			break;
-		case 'ArrowDown':
-			acceleration.current = { x: 0, y: 0 };  // To stop acceleration
-			break;
-case ' ':
-			  const bulletOffsetX = 4;  // Adjust this as per your requirement
-			  const bulletOffsetY = 20;  // Adjust this as per your requirement
-			  setBullets(bullets => [
-				...bullets,
-				{ 
-				  id: Date.now(), 
-				  x: x + spaceship.current.offsetWidth / 2 - bulletOffsetX, 
-				  y: y + spaceship.current.offsetHeight / 2 - bulletOffsetY,
-				  rotation, 
-				  hit: false 
-				}
-			  ]);
-			  break;
-		default:
-		  break;
-	  }
-	}
-	
-  const handleKeyUp = (e) => {
-	  e.preventDefault();
-	  switch(e.key) {
-		case 'ArrowUp':
-		  setThrust(0);
-		  break;
-	    default:
-		  break;
-	  }
-	}
-	window.addEventListener('keydown', handleKeyDown);
-	window.addEventListener('keyup', handleKeyUp);
-	
-	return () => {
-	  window.removeEventListener('keydown', handleKeyDown);
-	  window.removeEventListener('keyup', handleKeyUp);
-	}
-	}
-	  }, [gameState, rotation, x, y]);    
- 
- useEffect(() => {
-   const intervalId = setInterval(() => {
-	 const spaceshipRect = spaceship.current.getBoundingClientRect();
- 
-	 const highlightElements = document.getElementsByClassName('highlight');
-	 for (let i = 0; i < highlightElements.length; i++) {
-	   if (highlightElements[i].classList.contains('hit') || highlightElements[i].classList.contains('collided')) {
-		 continue; // Skip the element if it already has the hit class or it has already collided
-	   }
- 
-	   const wordRect = highlightElements[i].getBoundingClientRect();
-	   // Check for collision
-	   if (
-		 wordRect.left < spaceshipRect.right &&
-		 wordRect.right > spaceshipRect.left &&
-		 wordRect.top < spaceshipRect.bottom &&
-		 wordRect.bottom > spaceshipRect.top
-	   ) {
-		 highlightElements[i].classList.add('collided'); // Mark the element as having collided
-		 setLives(lives => lives - 1);
-		 // collision detected, invert colors
-		 document.body.classList.add('invert');
-		 setTimeout(() => {
-		   document.body.classList.remove('invert');
-		 }, 500);
-	   }
-	 }
-   }, 50); // Check every 50ms
- 
-   return () => {
-	 clearInterval(intervalId);
-   }
- }, []);
-
-  
-  
-  // add a new useEffect to update position with velocity and acceleration
-useEffect(() => {
 	const intervalId = setInterval(() => {
-	  // Create a new acceleration vector at the current angle of rotation
-	  acceleration.current.x = Math.sin(rotation * (Math.PI / 180)) * thrust;
-	  acceleration.current.y = -Math.cos(rotation * (Math.PI / 180)) * thrust;
-  
-	  // Increase velocity by acceleration
-	  velocity.current.x += acceleration.current.x;
-	  velocity.current.y += acceleration.current.y;
-  
-	  // Decrease velocity by a small amount to simulate friction
-	  velocity.current.x *= 0.99;
-	  velocity.current.y *= 0.99;
-  
-	  let newX = x + velocity.current.x;
-	  let newY = y + velocity.current.y;
-  
-	  // If spaceship exits right, reappear on left
-	  if (newX > window.innerWidth) {
-		newX = 0;
-	  } 
-	  // If spaceship exits left, reappear on right
-	  else if (newX < 0) {
-		newX = window.innerWidth;
+
+	  const spaceshipRect = spaceship.current.getBoundingClientRect();
+
+	  const highlightElements = document.getElementsByClassName('highlight');
+	  for (let i = 0; i < highlightElements.length; i++) {
+		if (highlightElements[i].classList.contains('hit') || highlightElements[i].classList.contains('collided')) {
+		  continue; // Skip the element if it already has the hit class or it has already collided
+		}
+
+		const wordRect = highlightElements[i].getBoundingClientRect();
+		// Check for collision
+		if (
+		  wordRect.left < spaceshipRect.right &&
+		  wordRect.right > spaceshipRect.left &&
+		  wordRect.top < spaceshipRect.bottom &&
+		  wordRect.bottom > spaceshipRect.top
+		) {
+		  highlightElements[i].classList.add('collided'); // Mark the element as having collided
+		  setLives(lives => lives - 1);
+		  // collision detected, invert colors
+		  document.body.classList.add('invert');
+		  setTimeout(() => {
+			document.body.classList.remove('invert');
+		  }, 500);
+		}
 	  }
-  
-	  // If spaceship exits bottom, reappear on top
-	  if (newY > window.innerHeight) {
-		newY = 0;
-	  } 
-	  // If spaceship exits top, reappear on bottom
-	  else if (newY < 0) {
-		newY = window.innerHeight;
-	  }
-  
-	  setX(newX);
-	  setY(newY);
-	}, 50);
-  
+	}, 50); // Check every 50ms
+
 	return () => {
 	  clearInterval(intervalId);
-	};
-  }, [x, y, rotation, thrust]);
+	}
+  }, []);
 
-useEffect(() => {
+
+
+  // Display game over message when lives run out
+  useEffect(() => {
 	if (lives <= 0) {
 	  // Fade to black and show "GAME OVER"
 	  document.body.classList.add('game-over');
 	}
   }, [lives]);
 
-
+  // Handle bullets (create, move, hit, remove)
   useEffect(() => {
-	  if (gameState === "running") {
-  
-		const intervalId = setInterval(() => {
-		  setBullets((bullets) =>
-			bullets
-			  .map((bullet) => {
-				// Skip the bullet if it already hit a target
-				if (bullet.hit) {
-				  // Set a timeout to remove bullet after a short delay
-				  setTimeout(() => {
-					bullet.remove = true;
-				  }, 200); // 200 ms delay, adjust as needed
-				  return bullet;
-				}
-	
-				const highlightElements = document.getElementsByClassName('highlight');
-				for (let i = 0; i < highlightElements.length; i++) {
-				  if (highlightElements[i].classList.contains('hit')) {
-					// Skip the check if word is already hit
-					continue;
-				  }
-				  const rect = highlightElements[i].getBoundingClientRect();
-				  if (
-					bullet.x >= rect.left &&
-					bullet.x <= rect.right &&
-					bullet.y >= rect.top &&
-					bullet.y <= rect.bottom
-				  ) {
-					bullet.hit = true;
-					highlightElements[i].classList.add('hit'); // Add the 'hit' class
-	
-					// Add score based on the type of the word
-					if (highlightElements[i].classList.contains('highlight')) {
-					  setScore((score) => score + 5);
-					} else if (highlightElements[i].classList.contains('em')) {
-					  setScore((score) => score + 10);
-					} else if (highlightElements[i].classList.contains('strong')) {
-					  setScore((score) => score + 15);
-					}
-	
-					break;
-				  }
-				}
-	
-				return bullet;
-			  })
-			  .filter(
-				(bullet) =>
-				  // Ensure bullets that are marked for removal are filtered out
-				  !bullet.remove &&
-				  bullet.x >= 0 &&
-				  bullet.x <= window.innerWidth &&
-				  bullet.y >= 0 &&
-				  bullet.y <= window.innerHeight
-			  )
-		  );
-		}, 20); // Adjust as needed
-	
-		return () => {
-		  clearInterval(intervalId);
-		};
-	  }
-	}, [gameState]);
+	if (gameState === "running") {
 
-return (
+	  const intervalId = setInterval(() => {
+		setBullets((bullets) =>
+		  bullets
+			.map((bullet) => {
+			  // Skip the bullet if it already hit a target
+			  if (bullet.hit) {
+				// Set a timeout to remove bullet after a short delay
+				setTimeout(() => {
+				  bullet.remove = true;
+				}, 200); // 200 ms delay, adjust as needed
+				return bullet;
+			  }
+
+			  const highlightElements = document.getElementsByClassName('highlight');
+			  for (let i = 0; i < highlightElements.length; i++) {
+				if (highlightElements[i].classList.contains('hit')) {
+				  // Skip the check if word is already hit
+				  continue;
+				}
+				const rect = highlightElements[i].getBoundingClientRect();
+				if (
+				  bullet.x >= rect.left &&
+				  bullet.x <= rect.right &&
+				  bullet.y >= rect.top &&
+				  bullet.y <= rect.bottom
+				) {
+				  bullet.hit = true;
+				  highlightElements[i].classList.add('hit'); // Add the 'hit' class
+
+				  // Add score based on the type of the word
+				  if (highlightElements[i].classList.contains('highlight')) {
+					setScore((score) => score + 5);
+				  } else if (highlightElements[i].classList.contains('em')) {
+					setScore((score) => score + 10);
+				  } else if (highlightElements[i].classList.contains('strong')) {
+					setScore((score) => score + 15);
+				  }
+
+				  break;
+				}
+			  }
+
+			  return bullet;
+			})
+			.filter(
+			  (bullet) =>
+				// Ensure bullets that are marked for removal are filtered out
+				!bullet.remove &&
+				bullet.x >= 0 &&
+				bullet.x <= window.innerWidth &&
+				bullet.y >= 0 &&
+				bullet.y <= window.innerHeight
+			)
+		);
+	  }, 20); // Adjust as needed
+
+	  return () => {
+		clearInterval(intervalId);
+	  };
+	}
+  }, [gameState]);
+
+  // Game render
+  return (
 	<div className="App-header">
 	  <ScrollingText text={text} scrollSpeed={.25} />
 	  <div className="spacecraft" ref={spaceship} style={{left: `${x}px`, top: `${y}px`, transform: `rotate(${rotation}deg)`}}>▲</div>
 
-{bullets.map(bullet => (
+	{bullets.map(bullet => (
 		<Bullet
 		  key={bullet.id}
 		  x={bullet.x}
@@ -299,13 +219,13 @@ return (
 	  <div className="score">Score: {score}</div>
 	  <div className="lives">Lives: {lives}</div>
 	  <div className={`game-over-message ${lives <= 0 ? 'visible' : ''}`}>
-	  	<h1>GAME OVER</h1>
+		<h1>GAME OVER</h1>
 		  <button onClick={restartGame} className={`restart-button ${lives <= 0 ? 'visible' : ''}`}>Read Again</button>
 	  </div>
-
 	</div>
   );
 
 };
 
+// Export Component
 export default GameSpace;
---
+++ src/components/useSpaceship.js
@@ -0,0 +1,134 @@
+import { useState, useRef, useEffect } from "react";
+import {useBullet} from "./useBullet"; 
+
+const useSpaceship = (setBullets) => {
+  const [rotation, setRotation] = useState(0);
+  const velocity = useRef({x: 0, y: 0});
+  const acceleration = useRef({x: 0, y: 0});
+  const speed = 0.05; // control this to change the spaceship's speed
+  const [thrust, setThrust] = useState(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+const [lives, setLives] = useState(3); // or any initial value you want
+  const spaceship = useRef(null);
+
+  const handleKeyDown = (e) => {
+	e.preventDefault();
+	switch(e.key) {
+	case 'ArrowLeft':
+		setRotation(rotation => rotation - 10);
+		break;
+	case 'ArrowRight':
+		setRotation(rotation => rotation + 10);
+		break;
+	case 'ArrowUp':
+		let radian = rotation * Math.PI / 180;
+		setThrust(0.1); // Begin applying thrust when the up arrow is pressed
+
+		acceleration.current = {
+			x: -speed * Math.sin(-radian),
+			y: -speed * Math.cos(-radian)
+		};
+		break;
+	case 'ArrowDown':
+		acceleration.current = { x: 0, y: 0 };  // To stop acceleration
+		break;
+	case ' ':
+
+	  	const bulletOffsetX = 4;
+	  	const bulletOffsetY = 20;
+	  	setBullets(bullets => [
+			...bullets,
+			{ 
+			id: Date.now(), 
+			x: x + spaceship.current.offsetWidth / 2 - bulletOffsetX, 
+			y: y + spaceship.current.offsetHeight / 2 - bulletOffsetY,
+			rotation, 
+			hit: false 
+			}
+	  	]);
+
+		break;	
+	default:
+		break;
+	}
+  }
+
+  const handleKeyUp = (e) => {
+	e.preventDefault();
+	switch(e.key) {
+	case 'ArrowUp':
+		setThrust(0);
+		break;
+	default:
+		break;
+	}
+  }
+
+  // Continuously update spaceship position based on velocity and acceleration
+	useEffect(() => {
+	  const intervalId = setInterval(() => {
+		// Create a new acceleration vector at the current angle of rotation
+		acceleration.current.x = Math.sin(rotation * (Math.PI / 180)) * thrust;
+		acceleration.current.y = -Math.cos(rotation * (Math.PI / 180)) * thrust;
+  
+		// Increase velocity by acceleration
+		velocity.current.x += acceleration.current.x;
+		velocity.current.y += acceleration.current.y;
+  
+		// Decrease velocity by a small amount to simulate friction
+		velocity.current.x *= 0.99;
+		velocity.current.y *= 0.99;
+  
+		let newX = x + velocity.current.x;
+		let newY = y + velocity.current.y;
+  
+		// If spaceship exits right, reappear on left
+		if (newX > window.innerWidth) {
+		  newX = 0;
+		} 
+		// If spaceship exits left, reappear on right
+		else if (newX < 0) {
+		  newX = window.innerWidth;
+		}
+  
+		// If spaceship exits bottom, reappear on top
+		if (newY > window.innerHeight) {
+		  newY = 0;
+		} 
+		// If spaceship exits top, reappear on bottom
+		else if (newY < 0) {
+		  newY = window.innerHeight;
+		}
+  
+		setX(newX);
+		setY(newY);
+	  }, 50);
+  
+	  return () => {
+		clearInterval(intervalId);
+	  };
+	}, [x, y, rotation, thrust]);
+	
+	
+  return {
+	rotation,
+	acceleration,
+	setRotation,
+	velocity,
+	speed,
+	thrust,
+	setThrust,
+	x,
+	setX,
+	y,
+	setY,
+	lives,   
+	setLives,
+	spaceship,
+	handleKeyDown,
+	handleKeyUp
+  }
+};
+
+export default useSpaceship;
