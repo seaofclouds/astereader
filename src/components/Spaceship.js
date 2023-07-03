cdd5cec2881615c76e857cb5918bbfc3d7e2fc02
seaofclouds <seaofclouds@users.noreply.github.com>
2023-07-03 02:35:40 -0700

    move speed and targets to params

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,8 +1,7 @@
 const Spaceship = ({ x, y, rotation, spaceship, reset }) => {
-  
-  
   return (
-	<div className="spacecraft" ref={spaceship} style={{ left: `${x}px`, top: `${y}px`, transform: `rotate(${rotation}deg)`}}>▲</div>
+  <div className="spacecraft" ref={spaceship} style={{ left: `${x}px`, top: `${y}px`, transform: `rotate(${rotation}deg)`}}>▲</div>
+    
   );
 }
 
